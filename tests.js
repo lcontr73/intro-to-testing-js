92a727724df98e33ce0c765f8cea39a261a2f91d
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-11 18:39:05 -0500

    corrected exercise 2

--- tests.js
+++ tests.js
@@ -17,9 +17,13 @@ describe('helloWorld', function() {
 describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
+    })
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+
 // }); it("should return a string 'Hello, Jane!' when executed", function() {
 //         expect(typeof sayHello('Jane')).toBe('string');
 // }); it("should return a string 'Hello, Alex!' when executred", function() {
 //         expect(sayHello("Alex")).toBe("Hello, Alex!")
     });
-});
