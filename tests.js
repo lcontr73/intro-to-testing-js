78c43e0ece3582ff84edc689b6212f09d8f62601
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-11 17:23:22 -0500

    exercise 3 test string

--- tests.js
+++ tests.js
@@ -17,5 +17,8 @@ describe('helloWorld', function() {
 describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
+}); it("should return a string 'Hello, Codeup!' when executed", function() {
+    expect(typeof sayHello()).toBe('string');
+
 });
 });
