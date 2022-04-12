ba095e935f4a0de958f43ac89a99fa7f7b59786e
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-12 10:03:11 -0500

    update through question 8

--- code.js
+++ code.js
@@ -2,16 +2,34 @@
 function helloWorld() {
     return "Hello, World!";
 }
-// function sayHello(name) {
-//      if(name === 'Alex'){
-//         return "Hello, Alex!";
-//      } else if (name === 'Pat'){
-//          return "Hello, Pat!";
-//      } else {
-//         return "Hello, Jane!";
-//     }
-// }
 
+// Questions 3-5
+// function sayHello(input) {
+//      if(input === 'Alex'){
+//         return "Hello, Alex!";
+//      } else if (input === 'Pat'){
+//          return "Hello, Pat!";
+//      } else if (input === 'Jane'){
+//         return "Hello, Jane!";
+//     } else {
+//             return "Hello, " + input + "!";
+// }};
+
+
+// Question 6
+// function sayHello(input) {
+//     return "Hello, " + input + "!";
+// };
+
+// Question 8
 function sayHello(input) {
-    return "Hello, " + input + "!";
+     if(input){
+         return "Hello, " + input + "!";
+     } else if (input === false){
+         return "Hello, World!"
+     // } else if (input !== false){
+     //     return "Hello, World!"
+     } else {
+         return "Hello, World!"
+}
 }
